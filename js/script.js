5b4b8a96e51ab9346da08744eae035d8421ae533
cristina-lopez <105756863+cristina-lopez@users.noreply.github.com>
2022-05-29 15:43:44 -0700

    showPage function created and tested.

--- js/script.js
+++ js/script.js
@@ -12,19 +12,58 @@ For assistance:
 */
 
 
-
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+const itemsPerPage = 9;
 
+function showPage(list, page) {
+   // index for the first and last student on the page
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = (page * itemsPerPage);
+
+  // select the element with a class of `student-list` and assign it to a variable
+  /* QUESTION - Can I use this instead of querySelector? Why is querySelector better in this case?
+  const studentList = document.getElementsByClassName('student-list')[0];
+  */
+   const studentList = document.querySelector('.student-list');
+
+  // set the innerHTML property of the variable you just created to an empty string
+  studentList.innerHTML = '';
+
+  // loop over the length of the `list` parameter
+    // inside the loop create a conditional to display the proper students
+      // inside the conditional:
+        // create the elements needed to display the student information
+        // insert the above elements
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         var studentItem = `
+            <li class="student-item cf">
+               <div class="student-details">
+                  <img class="avatar" src=${list[i].picture.large} alt="Profile Picture">
+                  <h3> ${list[i].name.first} ${list[i].name.last} </h3>
+                  <span class="email"> ${list[i].email} </span>
+               </div>
+               <div class="joined-details">
+                  <span class="date"> Joined ${list[i].registered.date} </span>
+               </div>
+            </li>`;
+         studentList.insertAdjacentHTML("beforeend", studentItem);
+      }
+   }
+}
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination() {
+   
+}
 
 
 // Call functions
