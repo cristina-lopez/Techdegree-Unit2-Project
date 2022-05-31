1c1a508d2c174db97aa0794ae3b481209da05c34
cristina-lopez <105756863+cristina-lopez@users.noreply.github.com>
2022-05-31 13:44:33 -0700

    Comments added. Search button added.

--- js/script.js
+++ js/script.js
@@ -2,42 +2,23 @@
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
-
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-
 const itemsPerPage = 9;
-
+/*
+`showPage` function
+This function creates and inserts/appends the elements needed to display a "page" of nine students.
+*/
 function showPage(list, page) {
    // index for the first and last student on the page
    const startIndex = (page * itemsPerPage) - itemsPerPage;
    const endIndex = (page * itemsPerPage);
 
-  // select the element with a class of `student-list` and assign it to a variable
-  /* QUESTION - Can I use this instead of querySelector? Why is querySelector better in this case?
-  const studentList = document.getElementsByClassName('student-list')[0];
-  */
+   /* QUESTION - Can I use the code below instead of querySelector? Why is querySelector better in this case?
+      const studentList = document.getElementsByClassName('student-list')[0];
+   */
+
+   // creates a list of students that will be shown on the given page
    const studentList = document.querySelector('.student-list');
-
-  // set the innerHTML property of the variable you just created to an empty string
-  studentList.innerHTML = '';
-
-  // loop over the length of the `list` parameter
-    // inside the loop create a conditional to display the proper students
-      // inside the conditional:
-        // create the elements needed to display the student information
-        // insert the above elements
+   studentList.innerHTML = '';
    for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i < endIndex) {
          var studentItem = `
@@ -57,13 +38,92 @@ function showPage(list, page) {
 }
 
 /*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+`addPagination` function
+This function creates and inserts/appends the elements needed for the pagination buttons
 */
 
-function addPagination() {
-   
+function addPagination(list) {
+   const numOfPages = Math.ceil(list.length/itemsPerPage);
+
+  // creates buttons 
+   const linkList = document.querySelector('.link-list');
+   linkList.innerHTML = "";
+   for (let i = 1; i <= numOfPages; i++) {
+      var button = `
+         <li>
+            <button type="button"> ${i} </button>
+         </li>`;
+      linkList.insertAdjacentHTML("beforeend", button);
+   }
+
+  // give the first pagination button a class of "active". If a button is clicked, 
+  // the "active class" is removed from the first button and set for the button 
+  // clicked. 'showPage' function is called.
+   document.querySelector('button').className = "active";
+   linkList.addEventListener('click', (e) => {
+      if (e.target.tagName === 'BUTTON') {
+         document.querySelector('.active').className = '';
+         e.target.className = "active";
+         showPage(list, e.target.textContent);
+      }
+   })
 }
 
-
 // Call functions
+showPage(data, 1);
+addPagination(data);
+
+/*
+`addSearch` function
+This function dynamically creates a search button
+*/
+function addSearch() {
+   const label = document.createElement('label');
+   label.setAttribute('for', 'search');
+   label.setAttribute('class', 'student-search');
+   const span = document.createElement('span');
+   span.textContent = 'Search by name';
+   label.appendChild(span);
+   const input = document.createElement('input');
+   input.setAttribute('id', 'search');
+   input.setAttribute('placeholder', 'Search by name...');
+   label.appendChild(input);
+   const button = document.createElement('button');
+   button.setAttribute('type', 'button');
+   const img = document.createElement('img');
+   img.setAttribute('src', 'img/icn-search.svg');
+   img.setAttribute('alt', 'Search icon');
+   button.appendChild(img);
+   label.appendChild(button);
+   const h2 = document.querySelector('header');
+   h2.append(label);
+}
+
+addSearch();
+
+function searchName(searchInput, names){
+   for (let i = 0; i < names.length; i++) {
+      names[i].className.remove('match');
+      var checkName = names[i].textContent.toLowerCase();
+      if (searchInput.value.length !== 0 && checkName.includes(searchInput.value.toLowerCase())) {
+        names[i].className = 'match';
+    }
+  }
+}
+
+const dataNames = [];
+for (let i=0; i<data.length; i++) {
+   dataNames[i] = data[i].name.first + data[i].name.last;
+}
+
+button = document.querySelector('button');
+input = document.querySelector('input');
+
+button.addEventListener('click', (e) => {
+   e.preventDefault();
+   searchName(input, dataNames);
+});
+
+button.addEventListener('keyup', (e) => {
+   searchName(input, dataNames);
+});
